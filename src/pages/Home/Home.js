99e316310cd05a154ef31be4f36e34a2bcdd6434
Outhman790 <tangerino2011@gmail.com>
2023-09-21 17:31:33 +0100

    added css vars

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,12 @@
-import React, {useState, useEffect} from 'react'
-import Home_SectionOne from './Home_SectionOne'
-import Header from '../../components/Header'
+import React, { useState, useEffect } from "react";
+import Header from "../../components/Header";
 
 function Home() {
   return (
     <>
-        <Header />
-        <Home_SectionOne />
+      <Header />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
