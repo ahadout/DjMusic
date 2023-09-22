212d7012d77526baef04a13da373a6e3f575f2e6
ahadout <mohamedahdoutt@gmail.com>
2023-09-22 18:48:36 +0100

    HomeSection_V0.1

--- src/pages/Home/Home_Section1.js
+++ src/pages/Home/Home_Section1.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react';
+import "../../assets/css/Home/Home_Section1.css";
 
 function Home_Section1() {
   return (
-    <div><h1>Home_Section1</h1></div>
+    <>
+      <div className="section">
+            <div className="box">
+                <h1>Big Heading</h1>
+                <p>Some small text here...</p>
+                <button className="black-btn">Button 1</button>
+                <button className="transparent-btn">Button 2</button>
+            </div>
+        </div>
+    </>
   )
 }
 
