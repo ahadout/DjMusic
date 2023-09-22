9aa2ba1a3c3dfb9e336e8ebe05d4a4c94409e98c
Outhman790 <tangerino2011@gmail.com>
2023-09-22 16:27:36 +0100

    adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,51 @@
 import React from "react";
-
+import "../assets/css/Footer.css";
 function Footer() {
   return (
     <footer>
-      <div>
-        <img src="../assets/images/logo.png" />
-        <div></div>
+      <div class="container">
+        <div id="logo_social_media-footer">
+          <img src="../assets/images/logo.png" class="logo-footer" />
+          <div class="social_media-footer">
+            <i></i>
+            <i></i>
+            <i></i>
+            <i></i>
+          </div>
+        </div>
+        <div id="join_us-footer">
+          <p>Join us and get the all our Updates!</p>
+          <div>
+            <label for="">Email</label>
+            <input id="email" placeholder="Your email here" />
+          </div>
+        </div>
+        <div id="pages-footer">
+          <ul>
+            <li>
+              <a>About us</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a>Our plans</a>
+            </li>
+            <li>
+              <a>Contact us</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a>Hard drives</a>
+            </li>
+            <li>
+              <a>Preview</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
