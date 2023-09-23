c103bcff8735d24f589e395718dcfc50468f05a5
Outhman790 <tangerino2011@gmail.com>
2023-09-23 08:22:16 +0100

    added Home_section2

--- src/pages/Home/Home_Section2.js
+++ src/pages/Home/Home_Section2.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "../../assets/css/Home/Home_Section2.css";
+import hard_drive_blue_bg from "../../assets/images/hard_drive_blue_bg.png";
+import cloud_drive_blue_bg from "../../assets/images/cloud_drive_blue_bg.jpg";
 
 function Home_Section2() {
   return (
-    <div>Home_Section2</div>
-  )
+    <section id="home_section2">
+      <h1>DJ musicHD plans</h1>
+      <p>
+        Discover the best offers from DJ musicHD, we have a multi plans for help
+        you to <br />
+        enjoy with your favourite music djs
+      </p>
+      <div class="plans">
+        <div class="hard_drives-plan">
+          <img src={hard_drive_blue_bg} />
+          <div class="hard_drives-info">
+            <h2>
+              Hard <br />
+              Drives
+            </h2>
+            <hr />
+            <div>
+              <p>
+                Embrace the Beat, Hold the Rhythm — Get Our DJ Albums on Viscal
+                Hard Drives!
+              </p>
+              <button>Learn more</button>
+            </div>
+          </div>
+        </div>
+        <div class="cloud_drives-plan">
+          <img src={cloud_drive_blue_bg} />
+          <div class="cloud_drives-info">
+            <h2>
+              Cloud <br />
+              Drives
+            </h2>
+            <hr />
+            <div>
+              <p>
+                Embrace the Beat, Hold the Rhythm — Get Our DJ Albums on Cloud
+                Drives!
+              </p>
+              <button>Learn more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Home_Section2
+export default Home_Section2;
